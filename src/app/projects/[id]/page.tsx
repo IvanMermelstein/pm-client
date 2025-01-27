5d94ce85b6d2af37788b745b8a1138089264951d
Iván Mermelstein <ivan.mermelstein@gmail.com>
2025-01-27 07:44:16 -0300

    Add TableView

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -4,6 +4,7 @@ import { use, useState } from "react";
 import BoardView from "../BoardView";
 import ListView from "../ListView";
 import ProjectHeader from "../ProjectHeader";
+import TableView from "../TableView";
 import TimelineView from "../TimelineView";
 
 type Props = {
@@ -26,9 +27,17 @@ const Project = ({ params }: Props) => {
         activeTab === "List" &&
         <ListView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       }
-       {
+      {
         activeTab === "Timeline" &&
         <TimelineView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      }
+      {
+        activeTab === "Timeline" &&
+        <TimelineView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      }
+      {
+      activeTab === "Table" &&
+        <TableView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       }
     </div>
   )
